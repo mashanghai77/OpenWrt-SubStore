db2ba118e551f35dc25ed3a920c258d3316ceb23
第十六夜月 <mashanghai88@gmail.com>
2026-07-08 22:48:49 +0800

    Update postinstall.sh

--- luci-app-substore/root/usr/libexec/substore/postinstall.sh
+++ luci-app-substore/root/usr/libexec/substore/postinstall.sh
@@ -4,8 +4,9 @@ mkdir -p /etc/sub-store
 
 chmod +x /etc/init.d/substore
 /etc/init.d/substore enable
+/etc/init.d/substore start
 
-echo "Sub-Store installed. Enable it in Services > Sub-Store."
+echo "Sub-Store installed."
 
 rm -f /tmp/luci-indexcache* >/dev/null 2>&1
 rm -rf /tmp/luci-modulecache/* >/dev/null 2>&1
